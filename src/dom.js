83bf80512046814e47b9d5cadcb466c2a8c1740c
Sebastian Sala <sebastian_orko@hotmail.com>
2023-04-24 13:23:57 -0600

    ships class added & build board with ships

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,24 @@
 export function loadBoard(){
-    const content = document.getElementById('grid');
-    for(let row = 0; row < 8; row++){
-        for(let col = 0; col < 8; col++){
+    const grid1 = document.getElementById('grid');
+    const grid2 = document.getElementById('grid2');
+    for(let row = 0; row < 10; row++){
+        for(let col = 0; col < 10; col++){
             const cell = document.createElement('div');
+            const cell2 = document.createElement('div');
             cell.classList.add('bg-white');
             cell.classList.add('hover:bg-black');
-            content.appendChild(cell);
+            cell.classList.add('border');
+            cell.classList.add('border-black');
+            cell.classList.add('aspect-w-1');
+            cell.classList.add('aspect-h-1');
+            cell2.classList.add('bg-white');
+            cell2.classList.add('hover:bg-black');
+            cell2.classList.add('border');
+            cell2.classList.add('border-black');
+            cell2.classList.add('aspect-w-1');
+            cell2.classList.add('aspect-h-1');
+            grid1.appendChild(cell);
+            grid2.appendChild(cell2);
         }
     }
 }
--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,70 @@
-export class Gameboard{
-    constructor(){
-        this.buildBoard();
+class Node {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+}
+
+export class Gameboard {
+  constructor() {
+    this.board = this.buildBoard();
+    this.ships = [];
+  }
+
+  buildBoard() {
+    const board = [];
+    for (let row = 0; row < 10; row++) {
+      board[row] = [];
+      for (let col = 0; col < 10; col++) {
+        board[row][col] = new Node(row, col);
+      }
+    }
+    return board;
+  }
+
+  addShip(ship) {
+    if(ship.direction === 'horizontal'){
+        if(ship.length + ship.col > 10){
+            throw new Error('Max size limit');
+        }
+    }else{
+        if(ship.length + ship.row > 10){
+            throw new Error('Max size limit');
+        }
     }
 
-    buildBoard
+    for(let i = 0; i < this.ships.length; i++){
+        for(let j = 0; j < this.ships[i].length ; j++){
+            const currentShip = this.ships[i][j];
+            if(ship.direction === 'horizontal'){
+                if((ship.row + 1 === currentShip.row + 1 || ship.row - 1 === currentShip.row - 1) ||  (ship.col + 1 === currentShip.col + 1 || ship.col - 1 === currentShip.col - 1)){
+                    throw new Error('Cant place a ship on another ship!');
+                }
+            }else{
+                if((ship.row + 1 === currentShip.row + 1 || ship.row - 1 === currentShip.row - 1) ||  (ship.col + 1 === currentShip.col + 1 || ship.col - 1 === currentShip.col - 1)){
+                    throw new Error('Cant place a ship on another ship!');
+                }
+            }
+        }
+    }
+    this.ships.push(ship);
+  }
+}
+
+export class Ship {
+  constructor(row, col, length, direction) {
+    this.row = row;
+    this.col = col;
+    this.length = length;
+    this.direction = direction;
+    this.hits = 0;
+    this.sunk = this.isSunk();
+  }
+  isSunk() {
+    return this.hits === this.length ? true : false;
+  }
+
+  hit(){
+    return this.hits++;
+  }
 }
